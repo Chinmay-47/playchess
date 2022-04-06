88a62fa9afaa711db3c717374259bef0e3bee1ff
Chinmaya Srivatsa <csvatsa98@gmail.com>
2022-04-07 01:02:37 +0530

    added config.py for configuration settings

---
+++ src/playchess/config.py
@@ -0,0 +1,14 @@
+"""
+Configuration settings for playchess.
+"""
+
+
+WIDTH = 512     # Width of the chess board
+
+HEIGHT = 512    # Height of the chess board
+
+DIMENSIONS = 8  # Chess board is 8x8
+
+SQUARE_SIZE = HEIGHT // DIMENSIONS  # Size of each square
+
+MAX_FPS = 15    # For Animations
