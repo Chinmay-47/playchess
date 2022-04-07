35e40e2500e98398d4e4022024dbe75d9bc08cf3
Chinmaya Srivatsa <csvatsa98@gmail.com>
2022-04-07 12:15:19 +0530

    added more configurations

--- src/playchess/config.py
+++ src/playchess/config.py
@@ -3,12 +3,16 @@ Configuration settings for playchess.
 """
 
 
-WIDTH = 512     # Width of the chess board
-
-HEIGHT = 512    # Height of the chess board
+WIDTH = HEIGHT = 512     # Width and Height of the chess board
 
 DIMENSIONS = 8  # Chess board is 8x8
 
 SQUARE_SIZE = HEIGHT // DIMENSIONS  # Size of each square
 
 MAX_FPS = 15    # For Animations
+
+BACKGROUND_COLOUR = "white"     # Background color of the board
+
+LIGHT_SQUARE_COLOUR = "white"   # Colour of light squares
+
+DARK_SQUARE_COLOUR = "gray"   # Colour of light squares
