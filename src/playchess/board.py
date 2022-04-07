b59d9bfc15b5b9646e834ad820afc215e4c0d48a
Chinmaya Srivatsa <csvatsa98@gmail.com>
2022-04-08 01:02:26 +0530

    updated Board class to draw the board and pieces

--- src/playchess/board.py
+++ src/playchess/board.py
@@ -1,7 +1,9 @@
 from pprint import pprint
-from typing import Optional
+from typing import Optional, List
 
-from playchess.config import DIMENSIONS
+import pygame
+from playchess.config import DIMENSIONS, LIGHT_SQUARE_COLOUR, DARK_SQUARE_COLOUR, SQUARE_SIZE
+from playchess.images import CHESS_PIECE_IMAGES
 
 
 class Board:
@@ -10,18 +12,13 @@ class Board:
     """
 
     def __init__(self):
-        self.board = \
-            [["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"]] + \
-            [["bp"] * DIMENSIONS] + \
-            [[None] * DIMENSIONS] * (DIMENSIONS - 4) + \
-            [["wp"] * DIMENSIONS] + \
-            [["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]]
-        self.move_log = []
+        self.board: List[List[str]] = self._get_fresh_board_()
+        self.move_log: List[str] = []
 
     def print(self) -> None:
         pprint(self.board)
 
-    def moves_made(self, last_n: Optional[int] = None) -> list:
+    def moves_made(self, last_n: Optional[int] = None) -> List[str]:
         """Shows moves made previously"""
 
         if last_n is not None:
@@ -34,6 +31,47 @@ class Board:
         """Shows number of moves made."""
 
         return len(self.move_log)
+
+    @staticmethod
+    def _draw_board(screen: pygame.surface.Surface):
+        """Draws the board on a pygame screen."""
+
+        square_colours = [pygame.Color(LIGHT_SQUARE_COLOUR), pygame.Color(DARK_SQUARE_COLOUR)]
+
+        for row in range(DIMENSIONS):
+            for col in range(DIMENSIONS):
+
+                # Light squares occur when row + column is an even number
+                colour_to_draw = square_colours[((row + col) % 2)]
+                pygame.draw.rect(screen, colour_to_draw, pygame.Rect(col * SQUARE_SIZE,
+                                                                     row * SQUARE_SIZE,
+                                                                     SQUARE_SIZE, SQUARE_SIZE))
+
+    def _draw_pieces(self, screen: pygame.surface.Surface):
+        """Draws the chess pieces according to the board state."""
+
+        for row in range(DIMENSIONS):
+            for col in range(DIMENSIONS):
+                piece = self.board[row][col]
+
+                # Ignore empty squares
+                if piece != '--':
+                    screen.blit(CHESS_PIECE_IMAGES[piece], pygame.Rect(col * SQUARE_SIZE,
+                                                                       row * SQUARE_SIZE,
+                                                                       SQUARE_SIZE, SQUARE_SIZE))
+
+    @staticmethod
+    def _get_fresh_board_() -> List[List[str]]:
+        return \
+            [["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"]] + \
+            [["bp"] * DIMENSIONS] + \
+            [["--"] * DIMENSIONS] * (DIMENSIONS - 4) + \
+            [["wp"] * DIMENSIONS] + \
+            [["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]]
+
+    def draw(self, screen: pygame.surface.Surface):
+        self._draw_board(screen)
+        self._draw_pieces(screen)
 
 
 def main():
