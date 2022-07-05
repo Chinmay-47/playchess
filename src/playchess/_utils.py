989cdc254731452c0e6bf2c64c8309b8aaca1333
Chinmaya Srivatsa <csvatsa98@gmail.com>
2022-07-06 00:09:18 +0530

    minor config change

--- src/playchess/_utils.py
+++ src/playchess/_utils.py
@@ -1,5 +1,5 @@
 import pygame
-from playchess.config import WIDTH, HEIGHT, END_GAME_FONT_COLOUR, END_GAME_FONT_SIZE
+from playchess.config import BOARD_WIDTH, BOARD_HEIGHT, END_GAME_FONT_COLOUR, END_GAME_FONT_SIZE
 
 
 def draw_game_over_text(screen: pygame.surface.Surface, text: str) -> None:
@@ -8,7 +8,7 @@ def draw_game_over_text(screen: pygame.surface.Surface, text: str) -> None:
     font = pygame.font.Font(pygame.font.get_default_font(), END_GAME_FONT_SIZE)
     text_obj = font.render(text, False, pygame.Color(END_GAME_FONT_COLOUR))
 
-    text_loc_width = WIDTH/2 - text_obj.get_width()/2
-    text_loc_height = HEIGHT/2 - text_obj.get_height()/2
-    text_loc = pygame.Rect(0, 0, WIDTH, HEIGHT).move(text_loc_width, text_loc_height)
+    text_loc_width = BOARD_WIDTH / 2 - text_obj.get_width() / 2
+    text_loc_height = BOARD_HEIGHT / 2 - text_obj.get_height() / 2
+    text_loc = pygame.Rect(0, 0, BOARD_WIDTH, BOARD_HEIGHT).move(text_loc_width, text_loc_height)
     screen.blit(text_obj, text_loc)
