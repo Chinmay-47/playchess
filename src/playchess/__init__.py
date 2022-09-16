8b007d14735557024ffad2790df0ea091bfd9844
Chinmaya Srivatsa <csvatsa98@gmail.com>
2022-09-16 12:39:41 -0400

    fixed types for mypy

--- src/playchess/__init__.py
+++ src/playchess/__init__.py
@@ -7,24 +7,24 @@ class PlayChess:
     @staticmethod
     def play_material_bot():
         """Gets the most material advantage for 1 move with basic and minimal positional scoring."""
-        play(**{"player1": "human", "player2": "bot", "ai": "material"})
+        play(player1="human", player2="bot", ai="material")
 
     @staticmethod
     def play_human():
         """Play another human."""
-        play(**{"player1": "human", "player2": "human"})
+        play(player1="human", player2="human")
 
     @staticmethod
     def play_minmax_bot(pruning: bool = False, depth: int = 3):
         """Play against a MinMax bot with optional move pruning."""
 
         if not pruning:
-            play(**{"player1": "human", "player2": "bot", "ai": "minmax", "depth": depth})
+            play(player1="human", player2="bot", ai="minmax", depth=depth)
             return
 
-        play(**{"player1": "human", "player2": "bot", "ai": "minmax_pruned", "depth": depth})
+        play(player1="human", player2="bot", ai="minmax_pruned", depth=depth)
 
     @staticmethod
     def play_random_bot():
         """Play against a bot that makes random moves."""
-        play(**{"player1": "human", "player2": "bot", "ai": "random"})
+        play(player1="human", player2="bot", ai="random")
